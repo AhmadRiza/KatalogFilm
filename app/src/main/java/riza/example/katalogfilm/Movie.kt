5ebaab58a443fd797eb383937c227ace803648d1
AhmadRiza <riza.public@gmail.com>
2019-09-25 18:58:49 +0700

    rv

---
+++ app/src/main/java/riza/example/katalogfilm/Movie.kt
@@ -0,0 +1,12 @@
+package riza.example.katalogfilm
+
+/**
+ * Created by riza@deliv.co.id on 9/25/19.
+ */
+
+data class Movie(
+    val title: String,
+    val year: String,
+    val detail: String,
+    val poster: String
+)
---
+++ app/src/main/java/riza/example/katalogfilm/MovieAdapter.kt
@@ -0,0 +1,6 @@
+package riza.example.katalogfilm
+
+/**
+ * Created by riza@deliv.co.id on 9/25/19.
+ */
+
